0cb051fc34b0726f9f0852485bf1ad952cfe3f01
Jan Murawski <jmurawski@sigma.ug.edu.pl>
2012-12-09 20:39:32 +0100

    laborki nr 7

---
+++ lab7.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Rozwiazanie laboratorium
+# http://wbzyl.inf.ug.edu.pl/sp/labs07
+
+#1-------------------
+grep ".\{1,\}" plik.txt
+
+#2-------------------
+grep ^[0-9] pl*
+
+#3-------------------
+grep -l "r\{9\}" *
+
+#4-------------------
+grep bash$ /etc/passwd
+
+#5-------------------
+
